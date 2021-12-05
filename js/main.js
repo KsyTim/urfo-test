79601ceaf9b507348df98e4f0287d67687cc8ad1
KsyTim <timoshenko.ksenia.1998@gmail.com>
2021-12-06 00:29:21 +0400

    added responsive to equipment section and filters modal

--- js/main.js
+++ js/main.js
@@ -54,28 +54,94 @@ const inputLeft = document.getElementById("input-left"),
       })
     },
     filterElem = document.querySelector('.equipment__filter'),
-    modalOverlay = document.querySelector('.modal__overlay');
+    modalOverlay = document.querySelector('.modal__overlay'),
+    goodsContainer = document.querySelector('.equipment__goods'),
+    goodsItem = document.querySelectorAll('.equipment__items__item'),
+    equipmentItems = document.querySelector('.equipment__items'),
+    removeSale = (elem) => {
+      if (elem) {
+        const oldPrice = elem.querySelector('.equipment__items__price_old'),
+          saleInfo = elem.querySelector('.equipment__items__sale');
+        if (oldPrice && saleInfo) {
+          oldPrice.remove();
+          saleInfo.remove();
+        }  
+      } 
+    },
+    hideGoods = () => {
+      if (document.documentElement.offsetWidth <= 480) {
+        const moreButton = 
+        `
+          <div class="equipment__goods__more">
+            <a href="#" class="equipment__goods__more_show">Показать еще</a>
+          </div> 
+        `;
+        goodsItem.forEach((elem, index) => {
+          if (index > 7) {
+            elem.style.display = 'none';
+          } else if (index === 7) {
+            removeSale(elem);
+          }  
+        })
+        goodsContainer.insertAdjacentHTML('beforeend', moreButton);
+      } 
+    },
+    moreGoods = () => {
+      if (document.documentElement.offsetWidth <= 480) {
+        const firstItem = 
+        `
+        <div class="equipment__items__item">
+          <div class="equipment__items__content">
+            <img src="./img/sections/equipment/item_01.png" alt="Equipment: Item 01" class="equipment__items__pic">
+            <p class="equipment__items__title">Биметаллический радиатор Royal Thermo Indigo Super
+            </p>
+          </div>
+          <div class="equipment__items__cost">
+            <div class="equipment__items__price">
+              <span>4 250<sup>₽</sup></span>
+            </div>
+            <a class="equipment__items__cart">
+              <img src="./img/sections/equipment/cart.svg" alt="" class="equipment__items__cart-pic">
+            </a>
+          </div>
+        </div>
+        `;
+        for(let i = 0; i < 2; i++) {
+          equipmentItems.insertAdjacentHTML('beforeend', firstItem);
+        }
+        document.querySelectorAll('.equipment__items__item').forEach((elem, index) => {
+          if (index === 0) {
+            addSaleInfo(elem); 
+            elem.querySelector('.equipment__items__sale').remove();
+            
+          } 
+          elem.querySelector('.equipment__items__price span').textContent = '13 890';
 
-  setLeftValue();
-  setRightValue();
-  setSale();
+        })
+      }
+    };
 
-  inputLeft.addEventListener("input", setLeftValue);
-  inputRight.addEventListener("input", setRightValue);
-  document.addEventListener('click', (event) => {
-    const target = event.target;
-    if (target.matches('.equipment__info__img') || target.matches('.equipment__info__total') || target.matches('#equipment-filter')) {
-      modalOverlay.classList.add('modal__overlay_visible');
-      filterElem.style.display = 'block';
-      filterElem.classList.add('equipment__filter_hugescreen_visible');
-      document.querySelector('body').style.overflow = 'hidden';
-    } else if (target.matches('.modal_close') || target.matches('.modal__overlay_visible')) {
-      modalOverlay.classList.remove('modal__overlay_visible');
-      filterElem.style.display = 'none';
-      filterElem.classList.remove('equipment__filter_hugescreen_visible');
-      document.querySelector('body').style.overflow = 'unset';
-    } 
-  })
-  // window.addEventListener('resize', () => {
-    
-  // })
+setLeftValue();
+setRightValue();
+setSale();
+// hideGoods();
+moreGoods();
+
+inputLeft.addEventListener("input", setLeftValue);
+inputRight.addEventListener("input", setRightValue);
+document.addEventListener('click', (event) => {
+  const target = event.target;
+  if (target.matches('.equipment__info__img') || target.matches('.equipment__info__total') || target.matches('#equipment-filter')) {
+    modalOverlay.classList.add('modal__overlay_visible');
+    filterElem.style.display = 'block';
+    filterElem.classList.add('equipment__filter_hugescreen_visible');
+    document.querySelector('body').style.overflow = 'hidden';
+  } else if (target.matches('.modal_close') || target.matches('.modal__overlay_visible')) {
+    modalOverlay.classList.remove('modal__overlay_visible');
+    filterElem.style.display = 'none';
+    filterElem.classList.remove('equipment__filter_hugescreen_visible');
+    document.querySelector('body').style.overflow = 'unset';
+  } 
+})
+
+// window.addEventListener('resize', hideGoods);
